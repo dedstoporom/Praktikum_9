c4723fdee917ccd49b990fd7f18325ed6ddb509d
Maxim Rumyantsev <maxrumyant@mail.ru>
2021-12-19 20:44:14 +0300

    fix

--- Praktikum_9/Praktikum_9.cpp
+++ Praktikum_9/Praktikum_9.cpp
@@ -59,45 +59,48 @@ bool Deque<T>::PopBack()
 {
     if (size_ > 0) {
 
-
         return true;
     }
     else return false;
 }
 
-int main(){
-    int action=0;
+int main() {
+    int action = 0;
     int count;
     Deque<int> dec;
     cout << "Push Back[1]" << endl;
     cout << "Push Front[2]" << endl;
+    cout << "PopFront [5]" << endl;
     cout << "Size [3]" << endl;
     cout << "Exit [4]" << endl;
     while (action != 4) {
-        
+
         cout << "Your action:";
         cin >> action;
-            if (action != 4 && action != 3) {
-                cout << "Value:";
-                cin >> count;
-            }
-            switch (action)
-            {
-            case (1): {
-                dec.PushBack(count);
-                break;
-            }
-            case (2): {
-                dec.PushFront(count);
-                break;
-            }
-            case (3): {
-                cout << dec.GetSize() << endl;
-                break;
-            }
-         
+        if (action != 4 && action != 3) {
+            cout << "Value:";
+            cin >> count;
         }
-      
+        switch (action)
+        {
+        case (1): {
+            dec.PushBack(count);
+            break;
+        }
+        case (2): {
+            dec.PushFront(count);
+            break;
+        }
+        case (3): {
+            cout << dec.GetSize() << endl;
+            break;
+        }
+        case (5): {
+            cout << dec.PopBack() << endl;
+            break;
+        }
+        }
+
     }
 }
 
